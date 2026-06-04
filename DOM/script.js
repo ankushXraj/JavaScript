a1f3d727d1e7d277e17304cf41394e348598ecf3
ANKUSH0480 <rajankush0480@gmail.com>
2026-06-04 12:43:28 +0530

    3/6 work

--- DOM/script.js
+++ DOM/script.js
@@ -1,17 +1,2 @@
-var bulb = document.querySelector("#bulb");
-var btn = document.querySelector("button"); 
- 
-var flag = 0 
-
-btn.addEventListener("click",function(){
-  if (flag == 0 ) {
-  bulb.style.backgroundColor = "yellow";  
-console.log("clicked") 
-flag = 1 } 
-
-       else {
-      bulb.style.backgroundColor = "transparent";  
-    console.log("again clicked") 
-    flag = 0 } 
-       
-})   
+var h  = document.querySelectorAll("h1")
+console.log(h)
